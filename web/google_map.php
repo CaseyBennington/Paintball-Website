acfe9bcdb41149474ed0d2de91d15bc5f2d1e3f8
Casey Bennington <cbenning@vt.edu>
2016-10-04 23:00:02 -0700

    google updates

--- web/google_map.php
+++ web/google_map.php
@@ -69,7 +69,7 @@ $page_title = 'Find the Nearest Field';
             }
             ?>
 
-            <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&libraries=places"></script>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1lhYipcYX_G-jutwBZYOoJ3pA96FSNgA&v=3.exp&sensor=true&libraries=places"></script>
             <script>
                 var map;
                 var infowindow;
