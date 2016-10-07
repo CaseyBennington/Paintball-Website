1c55948be4be3dbfdab36e3418b626f300d5cdf9
Casey Bennington <cbenning@vt.edu>
2016-10-07 03:41:42 -0700

    Language update

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ include ('includes/header.php');
 <h2>Paintball Time!</h2>
     <span><img src="./images/whatispaintball.jpg" NAME="graphics1" ALIGN=LEFT WIDTH=320 HEIGHT=200 BORDER=0></span>
     <span><img src="./images/paintball2.jpg" NAME="graphics2" ALIGN=RIGHT WIDTH=320 HEIGHT=200 BORDER=0></span>
-    <p><b>San Diego Paintball Club</b> is a non profit, no nonsense, bicycling club used to promote bicycling while riding bikes.
+    <p><b>San Diego Paintball Club</b> is a non profit, no nonsense, paintballing club used to promote paintballing while shooting paintballs.
     </p>
     <p>
         The great thing about this club is you can shoot people and not get in trouble. Invite that pesky neighbor, uptight boss, or even miserable ex.  What could be more fun?
